530dbd9a0d97af551907dec1dbf5e584cb3a2913
FrostyCake47 <47393739+FrostyCake47@users.noreply.github.com>
2022-12-03 23:14:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,87 @@
 from tkinter import *
+from tkinter import messagebox
 import tkinter
 import time
 import random
 
 m = tkinter.Tk()
 
+
+ROW = 5
+COLUMN = 5
 buttonMap = {}
-row = 3
-column = 5
-red = True
+boxWidth = 15
+score = 0
+
+appearable = True
+
+scoreText = Label(m, text = "Score: 0", font =("helvicta", 14))
+scoreText.grid(row=0, column=0)
+scoreText.pack()
+
+timeLeft =  60
+timeText = Label(m, text= "Time: 1:00", font=("helvicta", 14))
+timeText.pack()
+
+gamegrid = Frame(m)
+gamegrid.pack(side=BOTTOM)
+
+for i in range(ROW):
+    for j in range(COLUMN):
+        mybutton= Button(gamegrid, text=" "*boxWidth)
+        mybutton.grid(row=i+1, column=j)
+        buttonMap[mybutton] = (i+1, j)
+
+def removeFunc():
+    pass
+
+def makeActive(buttonDetail):
+    buttonDetail[0].config(bg= "red")
+    m.after(ms=200 ,func=makeActive(buttonDetail))
+
+def countdown(count):   
+    timeText['text'] = "Time: "+ str(count)
+    if count > 0:
+        m.after(1000, countdown, count-1)
+    else:
+        messagebox.showinfo("Game Over!", "Score: " + str(score))
+        m.quit()
+        
+
+def onCLick():
+    global appearable
+    global score
+    global scoreText
+
+    buttonDetail[0].config(bg = "white", command= removeFunc)
+    score += 1
+    appearable = True
+    stext = "Score: " + str(score)
+    scoreText.config(text=stext)
+
+    game()
+
+def game():
+    global appearable
+    global buttonDetail
+
+    if appearable:
+        print(buttonDetail[0])
+        buttonDetail = random.choice(list(buttonMap.items()))
+        buttonDetail[0].config(bg= "red", command=onCLick)
+        appearable = False
 
 
-def removeActive(buttonDetail):
-    buttonDetail[0].config(bg= "white")
-    m.after(ms=200, func=removeActive(buttonDetail))
-
-'''def makeActive(prevBlock= None):
+def main():
+    global buttonDetail
+    global appearable
+    count = 5
 
     buttonDetail = random.choice(list(buttonMap.items()))
+    appearable = True
+    countdown(count)
+    game()
+    m.mainloop()
 
-    buttonDetail[0].config(bg= "red")
-    prevBlock = buttonDetail
-    prevBlock[0].config(bg = "white")
-    print(buttonDetail[0])
-    m.after(ms=200 ,func=makeActive(prevBlock))'''
-
-def makeActive(prevBlock= None):
-
-    buttonDetail = random.choice(list(buttonMap.items()))
-
-    buttonDetail[0].config(bg= "red")
-    print(buttonDetail[0])
-    m.after(ms=200 ,func=makeActive)
-
-for i in range(row):
-    for j in range(column):
-        mybutton= Button(m, text="              ")
-        mybutton.grid(row=i, column=j)
-        buttonMap[mybutton] = (i, j)
-
-makeActive()
-
-m.mainloop()
-
-
-
-    
+if __name__ == "__main__":
+    main()
